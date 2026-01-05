688af8138987b164ed0d00928a791f7bfaf09372
ohmsl <ollie@nexdynamic.com>
2026-01-05 22:01:17 +0000

    fix: potential incompatibilities with mics

--- apps/daemon/src/audio/source/mic.rs
+++ apps/daemon/src/audio/source/mic.rs
@@ -35,16 +35,26 @@ impl MicAudioSource {
         src.set_property("do-timestamp", &true);
         src.set_property_from_str("device", &self.device_id);
 
-        let queue = gst::ElementFactory::make("queue")
-            .build()
-            .map_err(|_| io::Error::new(io::ErrorKind::Other, "missing queue element"))?;
-
-        queue.set_property("max-size-time", &100_000_000u64);
-        queue.set_property_from_str("leaky", "downstream");
-
         let convert = gst::ElementFactory::make("audioconvert")
             .build()
             .map_err(|_| io::Error::new(io::ErrorKind::Other, "missing audioconvert"))?;
+
+        let resample = gst::ElementFactory::make("audioresample")
+            .build()
+            .map_err(|_| io::Error::new(io::ErrorKind::Other, "missing audioresample"))?;
+
+        resample.set_property("quality", &10i32);
+
+        let capsfilter = gst::ElementFactory::make("capsfilter")
+            .build()
+            .map_err(|_| io::Error::new(io::ErrorKind::Other, "missing capsfilter"))?;
+
+        let caps = gst::Caps::builder("audio/x-raw")
+            .field("rate", 48_000i32)
+            .field("channels", 2i32)
+            .field("layout", "interleaved")
+            .build();
+        capsfilter.set_property("caps", &caps);
 
         let volume = gst::ElementFactory::make("volume")
             .build()
@@ -52,10 +62,17 @@ impl MicAudioSource {
         let volume_value = volume_value as f64;
         volume.set_property("volume", &volume_value);
 
+        let queue = gst::ElementFactory::make("queue")
+            .build()
+            .map_err(|_| io::Error::new(io::ErrorKind::Other, "missing queue element"))?;
+
+        queue.set_property("max-size-time", &100_000_000u64);
+        queue.set_property_from_str("leaky", "downstream");
+
         pipeline
-            .add_many(&[&src, &convert, &volume, &queue])
+            .add_many(&[&src, &convert, &resample, &capsfilter, &volume, &queue])
             .map_err(|_| io::Error::new(io::ErrorKind::Other, "failed to add elements"))?;
-        gst::Element::link_many(&[&src, &convert, &volume, &queue])
+        gst::Element::link_many(&[&src, &convert, &resample, &capsfilter, &volume, &queue])
             .map_err(|_| io::Error::new(io::ErrorKind::Other, "failed to link elements"))?;
 
         Ok(AudioSourceOutput {
